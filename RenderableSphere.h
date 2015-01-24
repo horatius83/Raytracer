4eb818b6764ed15b7aadfbf320dba7334e30cdbe
M Monster <horatius83@gmail.com>
2015-01-24 17:01:39 -0600

    Updated RenderableSphere
    Probably need to convert over to GLSL, though it would be sad to throw
    out all of that SIMD code I wrote...

--- RenderableSphere.h
+++ RenderableSphere.h
@@ -1,6 +1,8 @@
 #ifndef RENDERABLESPHERE_H
 #define RENDERABLESPHERE_H
 
+#include <tuple>
+#include <limits>
 #include "Sphere.h"
 #include "Renderable.h"
 #include "Vector.h"
@@ -10,21 +12,28 @@ namespace Graphics
 	class RenderableSphere : public Renderable 
 	{
 	public:
-		RenderableSphere(const Vector& center, float radius) {
+		RenderableSphere(const Math::Vector& center, float radius) {
 			_sphere = Math::Sphere(center, radius);
 		}
 
-		public Math::Vector GetMin() {
-			return Math::Vector(center.GetX() - radius, center.GetY() - radius, center.GetZ() - radius);
+		Math::Vector GetMin() {
+			return Math::Vector(_sphere.GetCenter().GetX() - _sphere.GetRadius(), 
+				_sphere.GetCenter().GetY() - _sphere.GetRadius(),
+				_sphere.GetCenter().GetZ() - _sphere.GetRadius());
 		}
 
-		return Math::Vector GetMax() {
+		Math::Vector GetMax() {
+			auto center = _sphere.GetCenter();
+			auto radius = _sphere.GetRadius();
 			return Math::Vector(center.GetX() + radius, center.GetY() + radius, center.GetZ() + radius);
 		}
 
-		public float GetIntersection(const Math::Vector& origin, const Math::Vector& direction) {
+		float GetIntersection(const Math::Vector& origin, const Math::Vector& direction) {
 			auto intersect = _sphere.GetIntersection(origin, direction);
-			return FLOAT_MAX;
+			if (std::get<0>(intersect)) {
+				return std::get<1>(intersect);
+			}
+			return std::numeric_limits<float>::max();
 		}
 	private:
 		Math::Sphere _sphere;
